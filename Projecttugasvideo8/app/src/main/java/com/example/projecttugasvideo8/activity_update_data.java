73597b6e70814b32401c0d0d28ca4bd2323a2956
Ayu <90167973+AyuPuss@users.noreply.github.com>
2021-12-02 22:42:12 +0700

    Add files via upload
    tugas ke 6

---
+++ Projecttugasvideo8/app/src/main/java/com/example/projecttugasvideo8/activity_update_data.java
@@ -0,0 +1,14 @@
+package com.example.projecttugasvideo8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_update_data extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_data);
+    }
+}
